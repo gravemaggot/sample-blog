8e6ef9699776d1dabe777ba9cc771df24cca8e93
Valdemar <gravemaggot1@gmail.com>
2020-09-15 22:24:14 +0300

    Tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,19 @@
 FactoryBot.define do 
-    factory :article do
-      title { 'Я, наконец-то начал дубасить рельсы.' }
-      text { "Как-то все не заходило, не заходило, а сейчас за уши не оттянешь" }
+  factory :article do
+    title { 'Я, наконец-то начал дубасить рельсы.' }
+    text { "Как-то все не заходило, не заходило, а сейчас за уши не оттянешь" }
+
+    factory :article_with_comments do
+      after :create do |article, _evaluator|
+        create_list :comment, 3, article: article
+      end
     end
+  end
+end
+
+FactoryBot.define do
+  factory :comment do
+    author { 'Mike' }
+    sequence(:body) { |n| "comment body #{n}" }
+  end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe Article do
-  describe 'validations' do 
+  describe 'validations' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
   end
@@ -19,6 +19,16 @@ describe Article do
 
       # assert checks
       expect(@article.subject).to eq 'Lorem Ipsum'
+    end
+  end
+
+  describe '#last_comment' do
+    it 'returns last comment' do
+      # create article with comments
+      @article = create(:article_with_comments)
+
+      # check last comment
+      expect(@article.last_comment.body).to eq 'comment body 3'
     end
   end
 end
